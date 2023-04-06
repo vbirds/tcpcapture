9be31eac5570f4143cf6fa76b236d83d9bbf2fdc
vbird <841952496@qq.com>
2023-04-06 14:31:23 +0800

    build: 更新插件模板
    更新插件模板和说明

--- plugins/plugin_def.h
+++ plugins/plugin_def.h
@@ -1,28 +1,3 @@
-
-## tcpcapture
-
-`tcpcapture` 是一个简单的 tcp 监听工具，可以用来监听 tcp 包，或者监听 tcp 包中的数据。支持自定义插件，可以用来解析自定义的协议。
-
-### 启动参数
-
-```bash
-
-Usage: ./tcpcapture [options] ...
-
-  -h, --help    Print help and exit
-  -V, --version    Print version and exit
-  -d, --device=STRING    device name, default use first eth device
-  -i, --ip=STRING    <ip address>
-  -p, --port=STRING    <port>
-  -P, --plugin=STRING    plugin shared library name
-
-```
-
-### 自定义插件开发
-
-支持动态库插件，在动态库的实现接口里解析自定义的协议。需要包含头文件 `plugin_def.h`，里面定义了动态库需要实现的接口，在cpp文件里实现这些接口，编译成动态库，然后在启动参数里指定动态库的名称即可。
-
-```cpp
 #pragma once
 
 #include <stdint.h>
@@ -71,4 +46,3 @@ API void plugin_close();
 #ifdef __cplusplus
 };
 #endif
-```
--- plugins/sample/library.h
+++ plugins/sample/library.h
@@ -1,19 +1,4 @@
 #pragma once
 
 #include <stdint.h>
-#include "tcpcapture.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-int plugin_init();
-void plugin_connection_start(connection_data* key);
-void plugin_connection_send(connection_data* key, const char* buffer, int len);
-void plugin_connection_recv(connection_data* key, const char* buffer, int len);
-void plugin_connection_close(connection_data* key);
-void plugin_close();
-
-#ifdef __cplusplus
-};
-#endif
+#include "plugin_def.h"
